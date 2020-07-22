6c3b66bbbc0f5d3e95aeaed4bf56c85dcba28f36
Purhan <official.purhan@gmail.com>
2020-07-22 21:04:39 +0530

    practice

---
+++ Submissions/CSES/weird_algorithm.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(int, i, a, b)        for (int i=a; i<(b); i++)
+#define forev(int, i, b, a)     for (int i = (b)-1; i >= a; i--)
+#define uns                     unsigned
+#define lli                     long long int
+#define ld                      long double
+#define all(x)                  x.begin(), x.end()
+#define sortall(x)              sort(all(x))
+#define enl                     "\n"
+#define dbg(x)                  cout << #x << ": " << x << enl;
+// #define PI 3.1415926535897932384626
+// const int MOD = 1000000007; const lli INF = 1e18; const int MX = 100001;
+
+void solve() {
+    lli n; cin >> n; cout << n << " ";
+    while (n != 1) {
+        if (!(n & 1)) {
+            n /= 2;
+        } else {
+            n = n + n + n + 1;
+        }
+        cout << n << " ";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    // int testcase; cin >> testcase; for (int i = 1; i <= testcase; ++i)
+    solve();
+    return 0;
+}
